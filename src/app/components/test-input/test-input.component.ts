c568ecef430dd1ae046fffdb30bf304be40763da
YuriiBoltukov <kolomiha898@gmail.com>
2022-12-06 21:01:48 +0300

    init gh-pages

--- src/app/components/test-input/test-input.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { FieldBase } from 'src/app/base/field.base';
-
-@Component({
-  selector: 'app-test-input',
-  templateUrl: './test-input.component.html',
-  styleUrls: ['./test-input.component.scss'],
-})
-export class TestInputComponent extends FieldBase {}
--- src/app/components/test-number/test-number.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { FieldBase } from 'src/app/base/field.base';
-
-@Component({
-  selector: 'app-test-number',
-  templateUrl: './test-number.component.html',
-  styleUrls: ['./test-number.component.scss'],
-})
-export class TestNumberComponent extends FieldBase {}
